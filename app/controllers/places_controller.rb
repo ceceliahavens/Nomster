e383e4b0516b592adcacfd8c44135ea22595d284
ceceliahavens <ceceliahavens@hotmail.com>
2015-09-28 15:57:32 +0000

    edit secured

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,36 +1,49 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, :only => [:new, :create]
+		before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
+	
 	def index
 		@places=Place.all
 	end
+
 	def index
 		@places = Place.order(:name).page(params[:page]).per(10)
 	end
+
 	def new
 		@place = Place.new
 	end 
+
 	def create
 		current_user.places.create(place_params)
 		redirect_to root_path 
 	end
+
 	def show
 		@place = Place.find(params[:id])
 	end
 
 	def edit
-    @place = Place.find(params[:id])
+    	@place = Place.find(params[:id])
+
+    	if @place.user != current_user
+    		return render :text => 'Not Allowed', :status => :forbidden
+    	end
 	end
 
 	def update
-  @place = Place.find(params[:id])
-   @place.update_attributes(place_params)
-     redirect_to root_path
+ 	 	@place = Place.find(params[:id])
+ 	 	if @place.user != current_user
+ 	 		return render :text => 'Not Allowed', :status => :forbidden
+ 	 	end
+ 	 	
+   		@place.update_attributes(place_params)
+     	redirect_to root_path
+ end
 
-     def destroy
-  @place = Place.find(params[:id])
-  @place.destroy
-  redirect_to root_path
-end
+    def destroy
+ 		 @place = Place.find(params[:id])
+  		@place.destroy
+  		redirect_to root_path
 end
 
 	private
