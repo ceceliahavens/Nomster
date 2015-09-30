d7779c88a4a6d78df0b3801a56dccaa2f6ec406a
ceceliahavens <ceceliahavens@hotmail.com>
2015-09-30 01:37:02 +0000

    validation

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,8 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
 	validates :name, :presence => true
+	validates :address, :presence => true
+	validates :description, :presence => true
+	validates_length_of :name, :maximum => 100 
+	validates_length_of :name, :minimum => 3
 end
