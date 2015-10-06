5fe7ad9ea2142c04bc198c2828ec76e24ebfceee
ceceliahavens <ceceliahavens@hotmail.com>
2015-10-06 00:34:13 +0000

    setting up photo db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151004000919) do
+ActiveRecord::Schema.define(version: 20151006001920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,17 @@ ActiveRecord::Schema.define(version: 20151004000919) do
 
   add_index "models", ["email"], name: "index_models_on_email", unique: true, using: :btree
   add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true, using: :btree
+
+  create_table "photos", force: true do |t|
+    t.text     "caption"
+    t.integer  "user_id"
+    t.integer  "place_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "photos", ["place_id"], name: "index_photos_on_place_id", using: :btree
+  add_index "photos", ["user_id", "place_id"], name: "index_photos_on_user_id_and_place_id", using: :btree
 
   create_table "places", force: true do |t|
     t.string   "name"
