ae9763f417d2565a1696509317da08779722e554
ceceliahavens <ceceliahavens@hotmail.com>
2015-10-13 23:55:26 +0000

    user profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Nomster::Application.routes.draw do
   devise_for :users
   devise_for :models
   root 'places#index'
-
   resources :places do
     resources :comments, :only => :create 
     resources :photos, :only => :create
   end
+  resources :users, :only => :show
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
